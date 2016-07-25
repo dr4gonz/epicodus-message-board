5793709a1dbbe8ee4fb1ce22f325f5e75d39d06e
Ben Logue and Matthew Reyes <ben+matthew@>
2016-07-25 15:52:59 -0700

    fix deletion in category.cs

--- Objects/Category.cs
+++ Objects/Category.cs
@@ -151,7 +151,8 @@ namespace MessageBoard
         string title = rdr.GetString(2);
         string mainText = rdr.GetString(3);
         int rating = rdr.GetInt32(4);
-        OriginalPost post = new OriginalPost(author, title, mainText, rating, id);
+        DateTime time = rdr.GetDateTime(5);
+        OriginalPost post = new OriginalPost(author, title, mainText, rating, time, id);
         allPosts.Add(post);
       }
       if (rdr != null) rdr.Close();
