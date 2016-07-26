0bed5109ea1919cfa19bd4adf0aeb042a7c96902
Carl Egbert and George Olson <carl+george@>
2016-07-26 11:39:47 -0700

    implent form showing/hiding to post page

--- Content/JS/script.js
+++ Content/JS/script.js
@@ -1,20 +1,19 @@
 $(document).ready(function(){
   $(".show-comment-form-top").click(function(){
+    $(".show-form").show();
     $(".show-comment-form-top").hide();
-    $(".child-reply").hide();
-    $(".reply-comment").show();
+    $("comment-form").hide();
     $(".comment-form-top").show();
   });
 
   $(".hide-comment-form-top").click(function(){
-    $(".show-comment-form-top").show();
+    $(".show-form").show();
     $(".comment-form-top").hide();
   });
 
   $(".show-comment-form-child").click(function(){
-    $(".show-comment-form-child").show();
-    $(".comment-form-top").hide();
-    $(".comment-form-child").hide();
+    $(".show-form").show();
+    $(".comment-form").hide();
     var id = this.id;
     $("#"+id+"-form").show();
     $("#"+id).hide();
@@ -22,18 +21,20 @@ $(document).ready(function(){
 
   $(".hide-comment-form-child").click(function(){
     $(".comment-form-child").hide();
-    $(".show-comment-form-child").show();
+    $(".show-form").show();
   });
 
-  $(".reply-comment-child").click(function(){
-    $(".reply-comment-form").hide();
-    $(".show-comment-form-top").show();
-    $(".comment-form-top").hide();
-    $(".comment-child-reply").hide();
+  $(".show-comment-form-grandchild").click(function(){
+    $(".show-form").show();
+    $(".comment-form").hide();
     var id = this.id;
-    $("#"+id+"-child-form").show();
-    $("#"+id+"-form").hide();
+    $("#"+id+"-form").show();
     $("#"+id).hide();
+  });
+
+  $(".hide-comment-form-grandchild").click(function(){
+    $(".comment-form-grandchild").hide();
+    $(".show-form").show();
   });
 
 });
